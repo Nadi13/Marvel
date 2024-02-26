19c7367f273f307b9594a15c12cde736e2ab8c15
Nadi13 <nadia_yudintseva@mail.ru>
2024-02-25 18:20:34 -0800

    made logo

---
+++ MarvelEffective/LocalTools/Color.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+public struct Colors {
+    public static let black = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 1.0)
+}
--- MarvelEffective/ViewController.swift
+++ MarvelEffective/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  MarvelEffective
-//
-//  Created by Nadi on 25.02.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var logo: UIImageView = {
+        let logo = UIImageView()
+        logo.image = UIImage(named: "logo")
+        logo.contentMode = .scaleToFill
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.heightAnchor.constraint(equalToConstant: 27).isActive = true
+        logo.widthAnchor.constraint(equalToConstant: 128).isActive = true
+        return logo
+    }()
+    
+    private func setImage(){
+        view.addSubview(logo)
+        logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        logo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = Colors.black
+        
+        setImage()
     }
-
-
+        
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
 
